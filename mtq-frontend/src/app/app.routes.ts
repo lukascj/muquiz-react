76d124bd7049c012690645eccb2971b43f58133f
Lukas J <lukascj04@gmail.com>
2024-08-20 15:44:28 +0200

    Initial commit

---
+++ mtq-frontend/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { HomeViewComponent } from './home-view/home-view.component';
+
+export const routes: Routes = [
+    { path: "", component: HomeViewComponent }
+];
