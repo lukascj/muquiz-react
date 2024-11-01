6dbe04600f9f28d698262487dfdc1dfb0f255144
Lukas Johansson <lukascj04@gmail.com>
2024-11-01 01:03:06 +0100

    påbörjat q&a content gen
    TODO:
    - drawQuestions
    - drawAnswers
    - eventuellt: Sheet component, AnswersContent component, QuestionsContent component

--- src/components/views/Quiz.js
+++ src/components/views/Quiz.js
@@ -10,10 +10,11 @@ const answerFormats = ['names', 'sheets', 'keys'];
 
 class Quiz {
   constructor(title, questions = [], answers = []) {
+    this.presets = presets;
     this.title = title;
     this.questions; // {notes: ["C#"], allowed_answers: [1,3,6], correct_answers: [1,6]} // allowed_answers keyword all
     this.answers; // {name: "Cmaj7", notes: ["C", "E", "G", "B"]}
-    this.presets = presets;
+    this.quizLen;
     if(this.presets.includes(title) && answers == [] && questions == []) {
       this.createFromPreset(title);
     } else {
@@ -24,6 +25,7 @@ class Quiz {
 
   setQuestions(questions) {
     this.questions = questions;
+    this.quizLen = questions.length;
   }
 
   setAnswers(answers) {
@@ -40,12 +42,40 @@ class Quiz {
     });
     shuffle(notes).forEach(note => { 
       answersCompressed = answers.map(a => a['name']);
-      questions.push({name: note, notes: [note], allowed_answers: 'all', correct_answers: findIndexes(note, answersCompressed)});
+      questions.push({
+        name: note, 
+        notes: [note], 
+        allowed_answers: 'all', 
+        correct_answers: findIndexes(note, answersCompressed)
+      });
     });
     return {questions: questions, answers: answers};
   }
 
   presetChords(musicData) {
+    let questions = [];
+    let answers = [];
+    let chords = [];
+    for(const type in musicData['chords']) {
+      chords.push(musicData['chords'][type]);
+    }
+    shuffle(chords).forEach(chord => {
+      const name = chord['name'].join(' ');
+      answers.push({name: name, notes: chord['notes']});
+    });
+    shuffle(musicData['notes']['white'].concat(musicData['notes']['black_sharp'])).forEach(chord => {
+      answersCompressed = answers.map(a => a['notes']);
+      questions.push({
+        name: chord['name'], 
+        notes: chord['notes'], 
+        allowed_answers: 'all', 
+        correct_answers: findIndexes(chord['notes'], answersCompressed)
+      });
+    });
+    return {questions: questions, answers: answers};
+  }
+
+  presetScales(musicData) {
     let questions = [];
     let answers = [];
     let scales = [];
@@ -57,21 +87,12 @@ class Quiz {
     });
     shuffle(scales).forEach(scale => {
       answersCompressed = answers.map(a => a['notes']);
-      questions.push({name: scale['name'], notes: scale['notes'], allowed_answers: 'all', correct_answers: findIndexes(scale['notes'], answersCompressed)});
-    });
-    return {questions: questions, answers: answers};
-  }
-
-  presetScales(musicData) {
-    let questions = [];
-    let answers = [];
-    shuffle(musicData['chords']).forEach(chord => {
-      const name = chord['name'].join(' ');
-      answers.push({name: name, notes: chord['notes']});
-    });
-    shuffle(musicData['notes']['white'].concat(musicData['notes']['black_sharp'])).forEach(chord => {
-      answersCompressed = answers.map(a => a['notes']);
-      questions.push({name: chord['name'], notes: chord['notes'], allowed_answers: 'all', correct_answers: findIndexes(chord['notes'], answersCompressed)});
+      questions.push({
+        name: scale['name'], 
+        notes: scale['notes'], 
+        allowed_answers: 'all', 
+        correct_answers: findIndexes(scale['notes'], answersCompressed)
+      });
     });
     return {questions: questions, answers: answers};
   }
@@ -101,81 +122,84 @@ class Quiz {
           .concat(resultChords.answers)
           .concat(resultScales.answers));
         break;
-      this.questions = result.questions;
-      this.answers = result.answers;
     }
+    this.setQuestions(result.questions);
+    this.setAnswers(result.answers);
   }
 }
 
 const params = new URLSearchParams(window.location.search);
 
-switch(params.get('p')) { // preset
-  case presets[1]: // notes
-    
-    break;
-  case presets[2]: // chords
-    
-    break;
-  case presets[3]: // scales
-    
-    break;
-  default: // all
-
-    break;
-}
-switch(params.get('q')) { // questions
-  case questionFormats[0]: // names
-    
-    break;
-  case questionFormats[1]: // sheets
-    
-    break;
-  default: // keys
-    
-    break;
-}
-switch(params.get('a')) { // answers
-  case questionFormats[1]: // sheets
-    
-    break;
-  case questionFormats[2]: // keys
-    
-    break;
-  default: // names
-    
-    break;
+let quiz;
+if(presets.includes(params.get('p'))) {
+  quiz = new Quiz(params.get('p'));
+} else {
+  throw new Error("Faulty preset.");
 }
 
-
-
-
-let quiz, quizSource, btns;
-if(params.get('v') == 'notes') {
-  quiz = {title: "Notes"}
-  notes = musicData['notes']['white'].concat(musicData['notes']['black']);
-  quiz.questions = []
-  shuffle(notes).forEach(note => {
-    quiz.questions
-  });
-  quiz = shuffle(quizSource);
-  btns = shuffle(quiz);
-} else if(params.get('v') == 'notes') {
-
+let questionsContent;
+function drawQuestions(format) {
+  let result;
+  switch(format) { // questions
+    case questionFormats[0]: // names
+      result = <>
+      </>;
+      break;
+    case questionFormats[1]: // sheets
+      result = <>
+      </>;
+      break;
+    default: // keys
+      result = <>
+      </>;
+      break;
+  }
+  return result;
 }
-const quizLength = quiz.length;
+if(questionFormats.includes(params.get('q'))) {
+  questionsContent = drawQuestions(params.get('q'));
+} else {
+  throw new Error("Faulty question-format.");
+}
+
+let answersContent;
+function drawAnswers(format) {
+  let result;
+  switch(format) { // answers
+    case answerFormats[1]: // sheets
+      result = <>
+      </>;
+      break;
+    case answerFormats[2]: // keys
+      result = <>
+      </>;
+      break;
+    default: // names
+      result = <>
+      </>;
+      break;
+  }
+  return result;
+}
+if(answerFormats.includes(params.get('a'))) {
+  answersContent = drawAnswers(params.get('a'));
+} else {
+  throw new Error("Faulty answer-format.");
+}
 
 export default function Quiz() {
-
+  // Handle quiz ending
   const [running, endQuiz] = useState(true);
   function stop() {
     endQuiz(!running);
   };
+  // Handle quiz progression
   const [questionNum, setQuestion] = useState(1);
   const answer = quiz[questionNum-1]['name'][0];
   function nextQuestion() {
     setQuestion(questionNum+1);
   };
-
+  // Handle score and high-score
   let [newHigh, setHigh] = useState(false);
   function updateHigh(s) {
     localStorage.setItem('highScore', s);
@@ -185,9 +209,9 @@ export default function Quiz() {
   function increaseScore() {
     setScore(score+1);
   };
-
   const highScore = localStorage.getItem('highScore') || 0;
 
+  // Also quiz progression
   const chooseAnswer = (event) => {
     const guess = event.target.textContent;
     if(guess == quiz[questionNum-1]['name'][0]) {
